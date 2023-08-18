2b4b77ea2f819bee27001700c34c64091dc042fe
musrex <leandro.n.lopez@gmail.com>
2023-08-18 19:49:58 -0400

    week6 starting week7

---
+++ week7/LopezL_Wk7.py
@@ -0,0 +1,16 @@
+# Task 1
+# Create a program that generates a random array of 10,000 elements
+# with equal number of 0's and 1's
+# and then implement the randomized Las Vegas algorithm
+# that searches for an element holding a 1
+# Your program should output the position of the first 1 found, plus the number of tries before finding it.
+
+ 
+
+# Task 2
+# Create a program that generates a random array of 10,000 elements
+# with equal number of 0's and 1's
+# and then implement the randomized Monte Carlo algorithm
+# that searches for an element holding a 1
+# Set k equal to 10
+# Your program should output the position of the first 1 found, plus the number of tries before finding it or a message stating that it did not find the 1 after k attempts.
